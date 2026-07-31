44913457b485542c5b44c558148ee2b2f4c6f562
Thanniru Yaswanth <121496542+Thanniru-yaswanth03@users.noreply.github.com>
2026-07-31 17:57:24 +0530

    Create 2048-BuildArrayFromPermutation

---
+++ https:/github.com/Thanniru-yaswanth03/LeetCode91.git/2048-BuildArrayFromPermutation/2048-BuildArrayFromPermutation.java
@@ -0,0 +1,11 @@
+// Last updated: 7/31/2026, 5:57:24 PM
+class Solution {
+    public int[] buildArray(int[] nums) {
+        int []ans= new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            ans[i] = nums[nums[i]];
+        }
+        return ans;
+
+    }
+}
