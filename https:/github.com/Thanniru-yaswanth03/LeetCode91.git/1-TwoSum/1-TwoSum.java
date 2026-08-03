1f216cebac991a638d1131afabb4a6f924a752da
Thanniru Yaswanth <121496542+Thanniru-yaswanth03@users.noreply.github.com>
2026-08-03 22:53:56 +0530

    Create 1-TwoSum

---
+++ https:/github.com/Thanniru-yaswanth03/LeetCode91.git/1-TwoSum/1-TwoSum.java
@@ -0,0 +1,14 @@
+// Last updated: 8/3/2026, 10:53:54 PM
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{}; // No solution found
+    }
+}
