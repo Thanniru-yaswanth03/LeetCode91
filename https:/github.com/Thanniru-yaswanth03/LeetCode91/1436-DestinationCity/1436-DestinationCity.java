5f959ad5de61829a1b9a66154a703adc515fa3db
Thanniru Yaswanth <121496542+Thanniru-yaswanth03@users.noreply.github.com>
2026-09-03 14:52:54 +0530

    File updated at 9/3/2026, 2:52:50 PM

--- https:/github.com/Thanniru-yaswanth03/LeetCode91/1436-DestinationCity/1436-DestinationCity.java
+++ https:/github.com/Thanniru-yaswanth03/LeetCode91/1436-DestinationCity/1436-DestinationCity.java
@@ -1,20 +1,16 @@
-// Last updated: 9/3/2026, 2:36:45 PM
+// Last updated: 9/3/2026, 2:52:50 PM
 1class Solution {
-2    public String destCity(List<List<String>> paths) {
-3        //what i did here is created a hashset called sources and then using a loop i added sources means left side cities to the hashset 
-4        HashSet<String> sources= new HashSet<>();
-5        for(int i=0;i<paths.size();i++){
-6            sources.add(paths.get(i).get(0));
-7        }
-8        //and using another loop i stored all my destinations like right side cities in a string called destination and then i compared them to souces like if my dest city is in the sources then it wont be my destination if it isnt there in the sources then that will be my final destinatin W bey 
-9        for(int i=0;i<paths.size();i++){
-10            String destination=paths.get(i).get(1);
-11            if(!sources.contains(destination)){
-12                return destination;
-13            }
-14        }
-15        //here its the edge case like return an empty string atleast bro if u got nothing to return coz i need ts so return a empty "" string as an edge case 
-16        return "";
-17        
-18    }
-19}
+2    public int largestAltitude(int[] gain) {
+3        //here what i did is created two vars alt and high and initialised them with 0 
+4        int highest=0;
+5        int altitude=0;
+6        //and ran a for loop and added  altitude with every ele till loop ends and then 
+7        for(int i=0;i<gain.length;i++){
+8            altitude+=gain[i];
+9            //here i checked whether my curr altitude is higher than the highest variable if it is then store it to return it later and its done 
+10            if(altitude>highest){
+11                highest=altitude;
+12            }
+13        }return highest;
+14    }
+15}
