9a903e40664a10af390078c7af306c7d08b2855e
Thanniru Yaswanth <121496542+Thanniru-yaswanth03@users.noreply.github.com>
2026-09-09 16:22:25 +0530

    File updated at 9/9/2026, 4:22:22 PM

--- https:/github.com/Thanniru-yaswanth03/LeetCode91/121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.java
+++ https:/github.com/Thanniru-yaswanth03/LeetCode91/121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.java
@@ -1,16 +1,35 @@
-// Last updated: 9/9/2026, 3:07:39 PM
-class Solution {
-    public int maxProfit(int[] prices) {
-        int maxProfit = 0;
-        int lowestPrice = Integer.MAX_VALUE;
-
-        for(int price : prices){
-            if(price < lowestPrice){
-                lowestPrice = price;
-            }
-            maxProfit = Math.max(maxProfit, price - lowestPrice);
-        }
-        System.gc();
-        return maxProfit;
-    }
-}
+// Last updated: 9/9/2026, 4:22:22 PM
+1class Solution {
+2    public int thirdMax(int[] nums) {
+3
+4        long first = Long.MIN_VALUE;
+5        long second = Long.MIN_VALUE;
+6        long third = Long.MIN_VALUE;
+7
+8        for (int num : nums) {
+9
+10            if (num == first || num == second || num == third) {
+11                continue;
+12            }
+13
+14            if (num > first) {
+15                third = second;
+16                second = first;
+17                first = num;
+18
+19            } else if (num > second) {
+20                third = second;
+21                second = num;
+22
+23            } else if (num > third) {
+24                third = num;
+25            }
+26        }
+27
+28        if (third == Long.MIN_VALUE) {
+29            return (int) first;
+30        }
+31
+32        return (int) third;
+33    }
+34}
