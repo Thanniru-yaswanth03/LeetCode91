e47e38f09dbfc31b5664f739949f660e8afc9da2
Thanniru Yaswanth <121496542+Thanniru-yaswanth03@users.noreply.github.com>
2026-09-10 11:08:16 +0530

    File updated at 9/10/2026, 11:08:16 AM

--- https:/github.com/Thanniru-yaswanth03/LeetCode91/905-SortArrayByParity/905-SortArrayByParity.java
+++ https:/github.com/Thanniru-yaswanth03/LeetCode91/905-SortArrayByParity/905-SortArrayByParity.java
@@ -1,14 +1,21 @@
-// Last updated: 9/10/2026, 11:07:14 AM
+// Last updated: 9/10/2026, 11:08:16 AM
 1class Solution {
-2    public void sortColors(int[] nums) {
-3        for(int i=0;i<nums.length;i++){
-4            for(int j=i+1;j<nums.length;j++){
-5                if(nums[j]<nums[i]){
-6                    int temp= nums[i];
-7                    nums[i]=nums[j];
-8                    nums[j]=temp;
-9                }
-10            }
-11        }
-12    }
-13}
+2
+3    public void sortColors(int[] nums) {
+4
+5        // Compare each element with the elements after it
+6        for (int i = 0; i < nums.length; i++) {
+7
+8            for (int j = i + 1; j < nums.length; j++) {
+9
+10                // If a smaller element is found, swap it with nums[i]
+11                if (nums[j] < nums[i]) {
+12
+13                    int temp = nums[i];
+14                    nums[i] = nums[j];
+15                    nums[j] = temp;
+16                }
+17            }
+18        }
+19    }
+20}
