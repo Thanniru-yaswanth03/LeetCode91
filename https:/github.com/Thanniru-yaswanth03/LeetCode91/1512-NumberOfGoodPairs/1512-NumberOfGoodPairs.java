26316b2260b3425b4d9052aeb501c6d0aff61faf
Thanniru Yaswanth <121496542+Thanniru-yaswanth03@users.noreply.github.com>
2026-09-13 19:13:30 +0530

    Create 1512-NumberOfGoodPairs

---
+++ https:/github.com/Thanniru-yaswanth03/LeetCode91/1512-NumberOfGoodPairs/1512-NumberOfGoodPairs.java
@@ -0,0 +1,16 @@
+// Last updated: 9/13/2026, 7:13:28 PM
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        int total = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    total++;
+                }
+            }
+        }
+
+        return total;
+    }
+}
