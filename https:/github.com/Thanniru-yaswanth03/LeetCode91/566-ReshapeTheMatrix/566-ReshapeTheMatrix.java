58532053c7f2f6422cc37957a9810f539f22ab26
Thanniru Yaswanth <121496542+Thanniru-yaswanth03@users.noreply.github.com>
2026-09-13 19:13:59 +0530

    Create 566-ReshapeTheMatrix

---
+++ https:/github.com/Thanniru-yaswanth03/LeetCode91/566-ReshapeTheMatrix/566-ReshapeTheMatrix.java
@@ -0,0 +1,20 @@
+// Last updated: 9/13/2026, 7:13:57 PM
+class Solution {
+    public int[][] matrixReshape(int[][] mat, int r, int c) {
+        int m = mat.length;
+        int n = mat[0].length;
+        int total = m * n;
+
+        if (r * c != total) {
+            return mat;
+        }
+
+        int[][] res = new int[r][c];
+
+        for (int i = 0; i < total; i++) {
+            res[i / c][i % c] = mat[i / n][i % n];
+        }
+
+        return res;
+    }
+}
